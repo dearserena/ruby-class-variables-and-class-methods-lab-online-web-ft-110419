d42d40780a3cc48e88125aaf959d4d07c0482bae
serena Lee <dearserenalee@gmail.com>
2019-11-26 05:56:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,11 @@ class Song
   genres = []
   
   def initialize(song_name, artist, genre )
+    @name = song_name
+    @artist = artist
+    @genre = genre
     @@count += 1
+    @@artists << artist
+    @@genres << genre
   end 
 end 
